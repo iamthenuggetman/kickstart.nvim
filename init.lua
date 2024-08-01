5298f9c457edaa15b88d1447bc1897f604bbf5f5
Tim McKeage <tim.mckeage@five9.com>
2024-08-01 12:08:30 -0400

    added more options to kanagwa colorscheme

--- init.lua
+++ init.lua
@@ -793,6 +793,31 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      require('kanagawa').setup {
+        compile = false, -- enable compiling the colorscheme
+        undercurl = true, -- enable undercurls
+        commentStyle = { italic = true },
+        functionStyle = {},
+        keywordStyle = { italic = true },
+        statementStyle = { bold = true },
+        typeStyle = {},
+        transparent = true, -- do not set background color
+        dimInactive = true, -- dim inactive window `:h hl-NormalNC`
+        terminalColors = true, -- define vim.g.terminal_color_{0,17}
+        -- colors = { -- add/modify theme and palette colors
+        --   palette = {},
+        --   theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+        -- },
+        -- overrides = function(colors) -- add/modify highlights
+        --   return {}
+        -- end,
+        -- theme = 'wave', -- Load "wave" theme when 'background' option is not set
+        -- background = { -- map the value of 'background' option to a theme
+        --   dark = 'wave', -- try "dragon" !
+        --   light = 'lotus',
+        -- },
+      }
+
       vim.cmd.colorscheme 'kanagawa'
 
       -- You can configure highlights by doing something like:
