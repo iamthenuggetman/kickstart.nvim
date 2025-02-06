fa65442a9cd462a6fccadfe37b509f33a9d371ac
Tim McKeage <tim@bearded.email>
2025-02-06 15:44:54 -0500

    updated neo-tree config and setup to use module plugins

---
+++ lua/custom/plugins/neo-tree.lua
@@ -0,0 +1,26 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  cmd = 'Neotree',
+  keys = {
+    { '<leader>fe', ':Neotree toggle<CR>', desc = 'NeoTree toggle', silent = true },
+  },
+  opts = {
+    filesystem = {
+      hijack_netrw_behavior = 'open_default', -- netrw disabled, opening a directory opens neo-tree
+      window = {
+        mappings = {
+          ['<leader>fe'] = 'close_window',
+        },
+      },
+    },
+  },
+}
