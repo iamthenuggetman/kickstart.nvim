47894fd231afefc8d7ec8624e391c52d37d3b519
Tim McKeage <tim@itmanic.com>
2025-03-12 07:10:26 -0400

    fixing friendly-snippets

--- init.lua
+++ init.lua
@@ -739,9 +739,9 @@ require('lazy').setup({
           return 'make install_jsregexp'
         end)(),
         dependencies = {
-          `friendly-snippets` contains a variety of premade snippets.
-             See the README about individual language/framework/plugin snippets:
-             https://github.com/rafamadriz/friendly-snippets
+          -- `friendly-snippets` contains a variety of premade snippets.
+          -- See the README about individual language/framework/plugin snippets:
+          --  https://github.com/rafamadriz/friendly-snippets
           {
             'rafamadriz/friendly-snippets',
             config = function()
