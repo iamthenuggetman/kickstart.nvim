af2ae8a022f3f6467f75ebb38eb54fe35d2124fa
Tim McKeage <tim.mckeage@five9.com>
2025-04-14 11:34:04 -0400

    updated colorscheme

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -4,7 +4,7 @@ return { -- You can easily change to a different colorscheme.
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
   'scottmckendry/cyberdream.nvim',
-  priority = 1000, -- Make sure to load this before all the other start plugins.
+  priority = 998, -- Make sure to load this before all the other start plugins.
 
   config = function()
     require('cyberdream').setup {
