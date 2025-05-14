e44e70994ad70fbacd2859e9d1046f5c9e69ca70
Tim McKeage <tim.mckeage@five9.com>
2025-05-14 13:00:52 -0400

    deleting some comments

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,8 +1,4 @@
-return { -- You can easily change to a different colorscheme.
-  -- Change the name of the colorscheme plugin below, and then
-  -- change the command in the config to whatever the name of that colorscheme is.
-  --
-  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+return {
   'scottmckendry/cyberdream.nvim',
   priority = 998, -- Make sure to load this before all the other start plugins.
 
